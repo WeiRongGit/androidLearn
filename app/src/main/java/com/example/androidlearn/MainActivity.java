b691341874154be8e5585facd998044bc7ccbfed
WeiRong <516902291@qq.com>
2017-12-18 16:15:51 +0800

    '了解活动，事件'

---
+++ app/src/main/java/com/example/androidlearn/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.androidlearn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frist_layout);
+
+
+        Toast.makeText(MainActivity.this, "hello world", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(MainActivity.this, "you press this add", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+            case R.id.delete_item:
+                Toast.makeText(MainActivity.this, "you press this delete", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+}
